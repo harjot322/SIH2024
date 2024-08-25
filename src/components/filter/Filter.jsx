b88ccac4078a23a996e05d70ed040d3f2ef65dbd
sehajmakkar <codewithsehaj@gmail.com>
2024-08-25 18:31:21 +0530

    list page and id pages

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,13 @@
+import './filter.scss';
+
+function Filter() {
+  return (
+    <div className="filter">
+      <div className="wrapper">
+        Filter
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
---
+++ src/routes/login/login.jsx
@@ -0,0 +1,12 @@
+import './login.scss'
+
+function Login(){
+
+  return(
+    <div className="login">
+      Login
+    </div>
+  )
+}
+
+export default Login
