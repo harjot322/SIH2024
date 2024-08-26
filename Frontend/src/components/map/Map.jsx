d05b307da135ae70f4dcc6d24f298534cc3e00ca
sehajmakkar <codewithsehaj@gmail.com>
2024-08-26 16:09:28 +0530

    map finished

--- Frontend/src/components/map/Map.jsx
+++ Frontend/src/components/map/Map.jsx
@@ -1,20 +1,18 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './map.scss'
 import "leaflet/dist/leaflet.css";
-import { Marker, Popup } from 'react-leaflet'
+import Pin from '../pin/Pin';
 
-function Map(){
+function Map({items}){
   return (
-    <MapContainer center={[28.7041, 77.1025]} zoom={13} scrollWheelZoom={false} className='map'>
+    <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[28.7041, 77.1025]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    {items.map(item=>(
+      <Pin item={item} key={item.id}/>
+    ))}
   </MapContainer>
   )
 }
--- Frontend/src/routes/listPage/listPage.jsx
+++ Frontend/src/routes/listPage/listPage.jsx
@@ -19,7 +19,7 @@ function ListPage() {
       </div>
 
       <div className="mapContainer">
-          <Map/>
+          <Map items={data} />
       </div>
     </div>
   )
