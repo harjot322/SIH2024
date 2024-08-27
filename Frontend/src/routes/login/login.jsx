81e7387928f6fbd09854c5d358e2ae2ef4279bbe
sehajmakkar <codewithsehaj@gmail.com>
2024-08-27 17:39:43 +0530

    login and register pages added! (styling needed)

--- Frontend/src/routes/login/login.jsx
+++ Frontend/src/routes/login/login.jsx
@@ -1,10 +1,37 @@
 import './login.scss'
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+
 
 function Login(){
 
   return(
     <div className="login">
-      Login
+      <div className="formContainer">
+        <form /*onSubmit={handleSubmit}*/>
+          <h1>Welcome back</h1>
+          <input
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button /*disabled={isLoading}*/>Login</button>
+          {/* {error && <span>{error}</span>} */}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
   )
 }
