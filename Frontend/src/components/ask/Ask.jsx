28a53bac2de3992bec41d258fa2cd771674e8039
sehajmakkar <codewithsehaj@gmail.com>
2024-08-31 02:53:57 +0530

    wiki ui done

--- Frontend/src/components/ask/Ask.jsx
+++ Frontend/src/components/ask/Ask.jsx
@@ -4,8 +4,44 @@ import { assets } from "../../assets/assets";
 
 function Ask() {
   return (
-    <div className="ask">
-      hello
+    <div className="main">
+      <div className="nav">
+        <p>FarmWiki</p>
+      </div>
+      <div className="main-container">
+        <div className="greet">
+            <p>Hello, Farmers</p>
+            <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Bhojpuri Gaane</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Gareebi Kaise Mitaaye</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Fasal Kharab Hogyi Kya kare?</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Biwi ko khet mein pakda kya kare?</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Prompt here.." />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
